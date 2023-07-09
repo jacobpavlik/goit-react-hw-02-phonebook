875fefd1bb589bff4e8dcade66071136fdbaf877
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-07-09 15:36:12 +0200

    Added filter components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -85,9 +85,28 @@ class ContactList extends Component {
   }
 }
 
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        <label>Find contatcs by name
+
+        <input type='text' name='filter' title='Filter contacts' /> 
+        </label>
+</div>
+    )
+  }
+}
+
+
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    filter:'',
     name: '',
     number:''
   };
@@ -104,7 +123,7 @@ export class App extends Component {
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} />
         <h2>Contacts </h2>
-
+        <Filter />
         <ContactList contacts={this.state.contacts} />
       </div>
     );
