9297a9ab99a6a8b132ac18c789c5c2e544009f65
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-07-10 21:27:15 +0200

    Added css module to Filter

--- src/components/Filter/filter.jsx
+++ src/components/Filter/filter.jsx
@@ -1,16 +1,17 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import css from './Filter.module.css';
 export class Filter extends Component {
   state = {
     filter: '',
   };
   render() {
     return (
-      <div>
-        <label>
+      <div className={css.filterContainer}>
+        <label className={css.filterLabel}>
           Find contacts by name
           <input
+            className={css.filterInput}
             onChange={this.props.handleFilter}
             type="text"
             name="filter"
