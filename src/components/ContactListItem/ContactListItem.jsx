94e10e123d5ed88c1f9484e516d8bc716f9cbb01
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-07-10 22:08:49 +0200

    Added css module to ContactListItem

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,64 +1,20 @@
 import PropTypes from 'prop-types';
-// import css from './ContactList.module.css';
-// import { Button } from '../button/Button';
+import css from './ContactListItem.module.css';
 
 export const ContactListItem = ({ contact, deleteContact, index }) => (
   <li>
-    <div>
-      <p>{contact.name}</p>
-      <p> {contact.number}</p>
+    <div className={css.listItemContainer}>
+      <p className={css.contactName}>{contact.name}:</p>
+
+      <p className={css.contactName}> {contact.number}</p>
+
+      <button className={css.deleteButton} onClick={() => deleteContact(index)}>
+        Delete
+      </button>
     </div>
-    <span>
-      <button onClick={() => deleteContact(index)}>Delete</button>
-    </span>
   </li>
 );
-
-//Piotr
-// import PropTypes from 'prop-types';
-// import css from './ContactList.module.css';
-// import { Button } from '../button/Button';
-
-// const ContactListItem = ({ contact, action }) => (
-//   <li className={css.item}>
-//     <div className={css.items_leftside}>
-//       <p className={css.info}>{contact.name}:</p>
-//       <a className={css.link} href={'tel:+' + contact.number}>
-//         {contact.number}
-//       </a>
-//     </div>
-//     <Button label="Delete" action={action} formButton={false} id={contact.id} />
-//   </li>
-// );
-
-//moje
-// class ContactList extends Component {
-//     render() {
-//       const { contacts, deleteContact, filter } = this.props;
-//       console.log('contacts i filter', contacts, filter);
-//       if (filter === '') {
-//         return (
-//           <div>
-//             <ul>
-//               {contacts.map((contact, index) => (
-//                 <li key={index}>
-//                   <span>{contact.name}</span>
-//                   <span>{contact.number}</span>
-//                   {/* {name}: {number} */}
-//                   deleteContact={deleteContact}
-//                   <span>
-//                     <button onClick={() => deleteContact(index)}>Delete</button>
-//                   </span>
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         );
-//       } else {
-//         return 'To jak filter nie jest pusty';
-//         // const filteredContacts = contacts.filter(contact =>
-//         //   contact.name.toLowerCase().includes(filter.toLowerCase())
-//         // );
-//       }
-//     }
-//   }
+ContactListItem.propTypes = {
+  contacts: PropTypes.array,
+  deleteContact: PropTypes.func,
+};
